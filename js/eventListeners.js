c7036d07f2eb7eec6ea30b70bbf9c346ade9a0cf
Divjot Singh <bogas04@gmail.com>
2015-10-17 20:13:35 +0530

    move keyboard shortcuts to other branch

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -21,27 +21,6 @@ module.exports = {
       dom.$baaniWrapper.innerHTML = t;
       dom.$baaniWrapper.focus();
       dom.$baaniWrapper.scrollTop = 0;
-      dom.$baaniWrapper.firstChild.className = 'hovered';
     }).catch(console.log)
-  },
-  $baaniWrapper: {
-    keydown: e => {
-      let hoveredElement = dom.$baaniWrapper.querySelector('.hovered') || dom.$baaniWrapper.firstChild;
-      // Capturing the strokes
-      switch(e.keyCode) {
-        case 40: case 74: // arrowDown & j
-          hoveredElement.classList.remove('hovered');
-          hoveredElement.nextElementSibling.className = 'hovered';
-          e.preventDefault(true);
-          break;
-        case 38: case 75: // arrowUp & k
-          hoveredElement.classList.remove('hovered');
-          hoveredElement.previousElementSibling.className = 'hovered';
-          e.preventDefault(true);
-          break;
-      }
-      // Proper scrolling
-      dom.$baaniWrapper.scrollTop = Math.max((hoveredElement.offsetTop - dom.$baaniWrapper.offsetHeight/2), 0);
-    }
   }
 };
