07fcebed4252420d3739092ea847ec2556b9cc66
Divjot Singh <bogas04@gmail.com>
2016-10-07 23:06:07 +0900

    update shabad

--- js/components/Shabad/index.js
+++ js/components/Shabad/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default ({ params }) => {
-  let shabad = null;
-
-  fetch('docs/keertan.json').then(r => r.json()).then(database => (
-    shabad = database.find(e => e.title === params.shabad)
-  ));
-
-  return (
-    <div style = {{height: '100%'}}>
-      <h3> This feature requires internet connection </h3>
-      <webview src = {shabad.url + '/print_view'} style = {{width: '100%'}} />
-    </div>
-  );
+export default class Shabad extends Component {
+  constructor(p) {
+    super(p);
+    this.state = { shabad: null };
+    fetch('docs/keertan.json').then(r => r.json()).then(database => {
+      shabad = database.find(e => e.title === params.shabad);
+      this.setState({ shabad });
+    });
+  }
+  render() {
+    return (
+      <div style = {{height: '100%'}}>
+        <h3> This feature requires internet connection </h3>
+        { this.state.shabad && <webview src = {this.state.shabad.url + '/print_view'} style = {{width: '100%'}} />}
+      </div>
+      );
+    }
 }
